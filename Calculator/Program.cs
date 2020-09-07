2e0337b84e72edacd8e7ef5c450a00897aec6c5f
mdorante <mdorante10@gmail.com>
2020-09-07 01:51:37 -0300

    add calculator functions (sum, subtract, multiply, divide)

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -11,14 +11,14 @@ namespace Calculator
         {
             bool notNumbers = true;
             int attemptsLeft = _MAX_ATTEMPTS_;
+            float num1 = 0;
+            float num2 = 0;
 
             while (notNumbers && attemptsLeft > 0)
             {
-                float num1;
                 Console.WriteLine("Please enter number:");
                 bool isNum1 = float.TryParse(Console.ReadLine(), out num1);
 
-                float num2;
                 Console.WriteLine("Please enter another number:");
                 bool isNum2 = float.TryParse(Console.ReadLine(), out num2);
 
@@ -44,12 +44,56 @@ namespace Calculator
                 return;
             }
 
-            Console.WriteLine("Select the operation you wish to execute:");
-            Console.WriteLine("1 - Sum");
-            Console.WriteLine("2 - Subtract");
-            Console.WriteLine("3 - Multiply");
-            Console.WriteLine("4 - Divide");
-            Console.WriteLine("0 - Exit");
+            int option;
+
+            do
+            {
+                Console.Clear();
+                Console.WriteLine("Select the operation you wish to execute:");
+                Console.WriteLine("1 - Sum");
+                Console.WriteLine("2 - Subtract");
+                Console.WriteLine("3 - Multiply");
+                Console.WriteLine("4 - Divide");
+                Console.WriteLine("0 - Exit");
+
+                bool isNum = int.TryParse(Console.ReadLine(), out option);
+
+                Console.Clear();
+                switch (option)
+                {
+                    case 0:
+                        Console.WriteLine("Exit...");
+                        break;
+                    case 1:
+                        Console.WriteLine($"{num1} + {num2} = {num1 + num2}");
+                        break;
+                    case 2:
+                        Console.WriteLine($"{num1} - {num2} = {num1 - num2}");
+                        break;
+                    case 3:
+                        Console.WriteLine($"{num1} x {num2} = {num1 * num2}");
+                        break;
+                    case 4:
+                        if (num2 != 0)
+                        {
+                            Console.WriteLine($"{num1} / {num2} = {num1 / num2}");
+                        }
+                        else
+                        {
+                            Console.WriteLine("Division by zero is not possible.");
+                        }
+                        break;
+                    default:
+                        Console.WriteLine("You did not select an available option.");
+                        break;
+                }
+
+                Console.WriteLine("Press enter to continue");
+                Console.ReadLine();
+                Console.Clear();
+            } while (option != 0);
+
+            Environment.Exit(0);
         }
     }
 }
